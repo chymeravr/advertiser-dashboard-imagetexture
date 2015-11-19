e1f913d8a8cd92d017620ce971851a77c1573715
p12sushilkm <sushilm.iitb@gmail.com>
2015-11-19 17:33:10 +0530

    Initial commit. Build a sample client server applications.

---
+++ src/main/java/co/chimeralabs/advertiser/server/apiDTO/AdResourceData.java
@@ -0,0 +1,37 @@
+package co.chimeralabs.advertiser.server.apiDTO;
+
+public class AdResourceData {
+	private String resourceURL;
+	private String resourceMetadata;
+	private String resourceErrorCode;
+	
+	public AdResourceData(){
+	}
+	
+	public AdResourceData(String resourceURL, String resourceMetadata, String resourceErrorCode){
+		this.resourceURL = resourceURL;
+		this.resourceMetadata = resourceMetadata;;
+		this.resourceErrorCode = resourceErrorCode;
+	}
+	
+	public String getResourceURL() {
+		return resourceURL;
+	}
+	public void setResourceURL(String resourceURL) {
+		this.resourceURL = resourceURL;
+	}
+	public String getResourceMetadata() {
+		return resourceMetadata;
+	}
+	public void setResourceMetadata(String resourceMetadata) {
+		this.resourceMetadata = resourceMetadata;
+	}
+	public String getResourceErrorCode() {
+		return resourceErrorCode;
+	}
+	public void setResourceErrorCode(String resourceErrorCode) {
+		this.resourceErrorCode = resourceErrorCode;
+	}
+	
+	
+}
---
+++ src/main/java/co/chimeralabs/advertiser/server/controller/PublisherAdAPIController.java
@@ -0,0 +1,24 @@
+package co.chimeralabs.advertiser.server.controller;
+
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RestController;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import co.chimeralabs.advertiser.server.apiDTO.AdResourceData;
+
+@RestController
+public class PublisherAdAPIController {
+	private static final Logger logger = LoggerFactory.getLogger(PublisherAdAPIController.class);
+	
+	@RequestMapping(value={"/publisher/api/loadad", "/"}, method=RequestMethod.GET, headers="Accept=application/json")
+	public AdResourceData getAdMetadata(){
+		logger.debug("Inside advertiserserver.publisheradapicontroller.getadmetadata");
+		String resourceURL = "http://www.chimeralabs.co";
+		String resourceMetadata = "jpeg";
+		String resourceErrorCode = "0";
+		AdResourceData adResourceData = new AdResourceData(resourceURL, resourceMetadata, resourceErrorCode);
+		return adResourceData;
+	}
+}
